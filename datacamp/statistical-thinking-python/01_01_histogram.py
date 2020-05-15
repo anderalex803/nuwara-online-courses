7c9442d10de5415a23b3e1122d117282812a84ac
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-15 18:41:04 +0700

    Create 01_01_histogram.py

---
+++ datacamp/statistical-thinking-python/01_01_histogram.py
@@ -0,0 +1,16 @@
+# Use iris dataset
+
+# Import numpy
+import numpy as np
+
+# Compute number of data points: n_data
+n_data = len(versicolor_petal_length)
+
+# Number of bins is the square root of number of data points: n_bins
+n_bins = np.sqrt(n_data)
+
+# Convert number of bins to integer: n_bins
+n_bins = int(n_bins)
+
+# Plot the histogram
+_ = plt.hist(versicolor_petal_length, bins=n_bins)
