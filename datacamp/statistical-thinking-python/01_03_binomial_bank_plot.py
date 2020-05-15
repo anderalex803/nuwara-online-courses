42b1c75a876fdf5011df8682a0079992a75b81be
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-16 00:33:57 +0700

    Create 01_03_binomial_bank_plot.py

---
+++ datacamp/statistical-thinking-python/01_03_binomial_bank_plot.py
@@ -0,0 +1,27 @@
+"Plotting the binomial ECDF"
+
+# Take 10,000 samples out of the binomial distribution: n_defaults
+n_defaults = np.random.binomial(100, 0.05, size=10000)
+
+# Compute CDF: x, y
+x, y = ecdf(n_defaults)
+
+# Plot the CDF with axis labels
+plt.plot(x, y, marker='.', linestyle='none')
+plt.xlabel('number of successes')
+plt.ylabel('cdf')
+
+"Plotting the binomial PMF"
+
+# Compute bin edges: bins
+bins = np.arange(0, max(n_defaults) + 1.5) - 0.5
+
+# Generate histogram
+plt.hist(n_defaults, normed=True, bins=bins)
+
+# Label axes
+plt.xlabel('number of successes')
+plt.ylabel('probability')
+
+# Show the plot
+plt.show()
