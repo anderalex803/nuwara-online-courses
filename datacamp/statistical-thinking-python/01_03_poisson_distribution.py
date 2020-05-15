d6fb0ee8a5a6016460e3427ecdb3fcd9a453b260
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-16 00:35:47 +0700

    Create 01_03_poisson_distribution.py

---
+++ datacamp/statistical-thinking-python/01_03_poisson_distribution.py
@@ -0,0 +1,14 @@
+# Draw 10,000 samples out of Poisson distribution: samples_poisson
+samples_poisson = np.random.poisson(10, size=10000)
+
+# Print the mean and standard deviation
+print('Poisson:     ', np.mean(samples_poisson),
+                       np.std(samples_poisson))
+
+# Specify values of n and p to consider for Binomial: n, p, so that n*p remains 10
+n = np.array([20, 100, 1000])
+p = np.array([0.5, 0.1, 0.01])
+
+# Draw 10,000 samples for each n,p pair: samples_binomial
+for i in range(3):
+    samples_binomial = np.random.binomial(n[i], p[i], size=10000)
