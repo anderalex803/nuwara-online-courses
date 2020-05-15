6213ad5474115ee1136919adae6368b8d3ae033d
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-16 00:21:45 +0700

    Update 01_03_perform_bernoulli_trials.py

--- datacamp/statistical-thinking-python/01_03_perform_bernoulli_trials.py
+++ datacamp/statistical-thinking-python/01_03_perform_bernoulli_trials.py
@@ -1,7 +1,3 @@
-"""
-You can think of a Bernoulli trial as a flip of a possibly biased coin. Specifically, each coin flip has a probability p of landing heads (success) and probability 1−p of landing tails (failure). In this exercise, you will write a function to perform n Bernoulli trials, perform_bernoulli_trials(n, p), which returns the number of successes out of n Bernoulli trials, each of which has probability p of success. To perform each Bernoulli trial, use the np.random.random() function, which returns a random number between zero and one.
-"""
-
 def perform_bernoulli_trials(n, p):
     """Perform n Bernoulli trials with success probability p
     and return number of successes."""
