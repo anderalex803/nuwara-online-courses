afcef04e1c2dfde18224cf9f4bbb7845755612b3
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-16 00:30:51 +0700

    Create 01_03_bank_mortgage_ecdf.py

---
+++ datacamp/statistical-thinking-python/01_03_bank_mortgage_ecdf.py
@@ -0,0 +1,15 @@
+from 01_01_ecdf import ecdf
+
+# Compute ECDF: x, y
+x, y = ecdf(n_defaults)
+
+# Plot the ECDF with labeled axes
+plt.plot(x, y, marker='.', linestyle='none')
+plt.xlabel('interest rate')
+plt.ylabel('cdf')
+
+# Show the plot
+plt.show()
+
+# Compute the number of 100-loan simulations with 10 or more defaults: n_lose_money
+n_lose_money = np.sum(n_defaults >= 10)
