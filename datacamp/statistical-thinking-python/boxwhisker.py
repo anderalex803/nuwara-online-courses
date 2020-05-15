0b537291d1b3ed5b2f4e247aee5f721348877814
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-15 21:22:07 +0700

    Create boxwhisker.py

---
+++ datacamp/statistical-thinking-python/boxwhisker.py
@@ -0,0 +1,13 @@
+# Create box-and-whisker plot
+
+import seaborn as sns
+
+# Create box plot with Seaborn's default settings
+sns.boxplot(x='species', y='petal length (cm)', data=df)
+
+# Label the axes
+plt.xlabel('species')
+plt.ylabel('petal length (cm)')
+
+# Show the plot
+plt.show()
