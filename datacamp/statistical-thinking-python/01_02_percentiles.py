3223af498de51d4cfac7abd7077b36f83b6aa9cc
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-15 21:21:03 +0700

    Create 01_02_percentiles.py

---
+++ datacamp/statistical-thinking-python/01_02_percentiles.py
@@ -0,0 +1,24 @@
+# Compute percentiles
+
+# Specify array of percentiles 2.5th, 25th, 50th, 75th, 97.5th: percentiles
+percentiles = np.array([2.5, 25, 50, 75, 97.5])
+
+# Compute percentiles: ptiles_vers
+ptiles_vers = np.percentile(versicolor_petal_length, percentiles)
+
+# Print the result
+print(ptiles_vers)
+
+# Plot the percentiles on ECDF chart
+
+# Plot the ECDF
+_ = plt.plot(x_vers, y_vers, '.')
+_ = plt.xlabel('petal length (cm)')
+_ = plt.ylabel('ECDF')
+
+# Overlay percentiles as red diamonds.
+_ = plt.plot(ptiles_vers, percentiles/100, marker='D', color='red',
+            linestyle='none')
+
+# Show the plot
+plt.show()
