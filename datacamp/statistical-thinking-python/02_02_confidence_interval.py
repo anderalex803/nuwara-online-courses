e6174f3e9a5d0c62b7803cd5999272f9e3b198c2
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-16 18:01:54 +0700

    Update 02_02_confidence_interval.py

--- datacamp/statistical-thinking-python/02_02_confidence_interval.py
+++ datacamp/statistical-thinking-python/02_02_confidence_interval.py
@@ -1,4 +1,3 @@
 # confidence interval e.g. 95% confidence is percentile at 2.5th and 97.5th of data
 
-lower_interval_limit = np.percentile(data, 2.5)
-upper_interval_limit = np.percentile(data, 97.5)
+print(np.percentile(data, [2.5, 97.5]))
