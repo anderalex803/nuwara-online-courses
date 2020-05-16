fbcae833c3b95fa136ebffa0df1bbbd95154a52c
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-16 17:48:17 +0700

    Create 02_02_bootstrapping_variance.py

---
+++ datacamp/statistical-thinking-python/02_02_bootstrapping_variance.py
@@ -0,0 +1,16 @@
+from 02_02_bootstrap import draw_bs_reps
+
+# Generate 10,000 bootstrap replicates of the variance: bs_replicates
+# func is compute var: np.var
+bs_replicates = draw_bs_reps(rainfall, np.var, size=10000)
+
+# Put the variance in units of square centimeters
+bs_replicates = bs_replicates / 100
+
+# Make a histogram of the results
+_ = plt.hist(bs_replicates, bins=50, normed=True)
+_ = plt.xlabel('variance of annual rainfall (sq. cm)')
+_ = plt.ylabel('PDF')
+
+# Show the plot
+plt.show()
