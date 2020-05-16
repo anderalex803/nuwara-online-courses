5ddac4dab11b95a6704319dc55e9478c8afceaee
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-16 17:44:58 +0700

    Create 02_02_bootstrapping_mean.py

---
+++ datacamp/statistical-thinking-python/02_02_bootstrapping_mean.py
@@ -0,0 +1,21 @@
+from 02_02_bootstrap import draw_bs_reps
+
+# Take 10,000 bootstrap replicates of the mean: bs_replicates
+# func to compute mean of data: just np.mean
+bs_replicates = draw_bs_reps(rainfall, func=np.mean, size=10000)
+
+# Compute and print SEM (standard error of mean of data)
+sem = np.std(rainfall) / np.sqrt(len(rainfall))
+print(sem)
+
+# Compute and print standard deviation of bootstrap replicates
+bs_std = np.std(bs_replicates)
+print(bs_std)
+
+# Make a histogram of the results
+_ = plt.hist(bs_replicates, bins=50, normed=True)
+_ = plt.xlabel('mean annual rainfall (mm)')
+_ = plt.ylabel('PDF')
+
+# Show the plot
+plt.show()
