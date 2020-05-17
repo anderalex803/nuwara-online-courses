732c7ead04aa00fe5a418337a0a417251e55b29e
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-17 16:01:11 +0700

    Create 01_lottery_break_even.py

---
+++ datacamp/statistical-simulation-python/01_lottery_break_even.py
@@ -0,0 +1,14 @@
+# Initialize simulations and cost of ticket
+sims, lottery_ticket_cost = 3000, 0
+
+# Use a while loop to increment `lottery_ticket_cost` till average value of outcomes falls below zero
+while 1:
+    outcomes = np.random.choice([-lottery_ticket_cost, grand_prize-lottery_ticket_cost],
+                 size=sims, p=[1-chance_of_winning, chance_of_winning], replace=True)
+    if outcomes.mean() < 0:
+        break
+    else:
+        lottery_ticket_cost += 1
+answer = lottery_ticket_cost - 1
+
+print("The highest price at which it makes sense to buy the ticket is {}".format(answer))
