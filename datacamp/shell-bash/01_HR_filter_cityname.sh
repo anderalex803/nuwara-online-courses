7c507aa5a50676f6640a4c055f7a78271e05b31e
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-18 01:36:37 +0700

    Create 01_HR_filter_cityname.sh

---
+++ datacamp/shell-bash/01_HR_filter_cityname.sh
@@ -0,0 +1,9 @@
+# a bash script without she-bang #!/bin/bash
+
+# Echo the first ARGV argument
+echo $1 
+
+# Cat all the files
+# Then pipe to grep using the first ARGV argument
+# Then write out to a named csv using the first ARGV argument
+cat hire_data/* | grep "$1" > "$1".csv
