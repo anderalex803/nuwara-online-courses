489ecdb295d1fd6c0f49a9f51c7e8c5164804191
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-17 14:18:27 +0700

    Create 01_dice_step3.py

---
+++ datacamp/statistical-simulation-python/01_dice_step3.py
@@ -0,0 +1,11 @@
+# Initialize number of dice, simulate & record outcome
+die, probabilities, num_dice = [1,2,3,4,5,6], [1/6, 1/6, 1/6, 1/6, 1/6, 1/6], 2
+outcomes = np.random.choice(die, size=2, p=probabilities) 
+
+# Win if the two dice show the same number
+if  outcomes[0] == outcomes[1]: 
+    answer = 'win' 
+else:
+    answer = 'lose'
+
+print("The dice show {} and {}. You {}!".format(outcomes[0], outcomes[1], answer))
