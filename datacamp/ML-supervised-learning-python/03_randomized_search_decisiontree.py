d3fa62f0042125edc76e30292aa38b16d7dadd3d
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-19 23:05:01 +0700

    Create 03_randomized_search_decisiontree.py

---
+++ datacamp/ML-supervised-learning-python/03_randomized_search_decisiontree.py
@@ -0,0 +1,21 @@
+"Apply Randomized Cross Validation to fit parameters of Decision Tree model"
+
+# Import necessary modules
+from scipy.stats import randint
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import RandomizedSearchCV
+
+# Setup the parameters and distributions to sample from: param_dist
+param_dist = {"max_depth": [3, None],
+              "max_features": randint(1, 9),
+              "min_samples_leaf": randint(1, 9),
+              "criterion": ["gini", "entropy"]}
+
+# Instantiate a Decision Tree classifier: tree
+tree = DecisionTreeClassifier()
+
+# Instantiate the RandomizedSearchCV object: tree_cv
+tree_cv = RandomizedSearchCV(tree, param_dist, cv=5)
+
+# Fit it to the data
+tree_cv.fit(X, y)
