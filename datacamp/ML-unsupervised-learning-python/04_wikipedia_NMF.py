2519c7a05c9908b0eab409b0fa327f8457036d96
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-20 16:58:54 +0700

    Update 04_wikipedia_NMF.py

--- datacamp/ML-unsupervised-learning-python/04_wikipedia_NMF.py
+++ datacamp/ML-unsupervised-learning-python/04_wikipedia_NMF.py
@@ -12,3 +12,17 @@ nmf_features = model.transform(articles)
 
 # Print the NMF features
 print(nmf_features)
+
+"Print the NMF of Anne Hathaway and Denzel Washington"
+
+# Import pandas
+import pandas as pd
+
+# Create a pandas DataFrame: df
+df = pd.DataFrame(nmf_features, index=titles)
+
+# Print the row for 'Anne Hathaway'
+print(df.loc['Anne Hathaway'])
+
+# Print the row for 'Denzel Washington'
+print(df.loc['Denzel Washington'])
