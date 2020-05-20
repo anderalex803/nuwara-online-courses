ddde603818585190989788e0ccaeb47f8bd7ebd8
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-20 17:50:39 +0700

    Create 04_cosine_similarity.py

---
+++ datacamp/ML-unsupervised-learning-python/04_cosine_similarity.py
@@ -0,0 +1,20 @@
+# The NMF feature (nmf_features) available in 04_wikipedia_NMF.py, while titles is a list of the article titles.
+
+# Perform the necessary imports
+import pandas as pd
+from sklearn.preprocessing import normalize
+
+# Normalize the NMF features: norm_features
+norm_features = normalize(nmf_features)
+
+# Create a DataFrame: df
+df = pd.DataFrame(norm_features, index=titles)
+
+# Select the row corresponding to 'Cristiano Ronaldo': article
+article = df.loc['Cristiano Ronaldo']
+
+# Compute the dot products: similarities
+similarities = df.dot(article)
+
+# Display those with the largest cosine similarity
+print(similarities.nlargest())
