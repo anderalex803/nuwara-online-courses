a3f1527339b56f10dc026a17290da4ac7f78c5c3
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-20 19:55:21 +0700

    Create 01_logreg_SVC_compare_accuracy.py

---
+++ datacamp/ML-linear-classifier-python/01_logreg_SVC_compare_accuracy.py
@@ -0,0 +1,17 @@
+# function LogisticRegression and SVC has been imported from sklearn
+
+from sklearn import datasets
+digits = datasets.load_digits()
+X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target)
+
+# Apply logistic regression and print scores
+lr = LogisticRegression()
+lr.fit(X_train, y_train)
+print(lr.score(X_train, y_train)) # training accuracy
+print(lr.score(X_test, y_test)) # validation accuracy
+
+# Apply SVM and print scores
+svm = SVC()
+svm.fit(X_train, y_train)
+print(svm.score(X_train, y_train))
+print(svm.score(X_test, y_test))
