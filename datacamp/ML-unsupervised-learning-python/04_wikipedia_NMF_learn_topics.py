c9a4fa8e67f1475545d30f960eb83dd44764fe33
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-20 17:17:46 +0700

    Create 04_wikipedia_NMF_learn_topics.py

---
+++ datacamp/ML-unsupervised-learning-python/04_wikipedia_NMF_learn_topics.py
@@ -0,0 +1,17 @@
+# this is linked to another script 03_wikipedia_NMF.py
+
+# Import pandas
+import pandas as pd
+
+# Create a DataFrame: components_df
+# the following "model" is from the another script 03_wikipedia_NMF.py
+components_df = pd.DataFrame(model.components_, columns=words)
+
+# Print the shape of the DataFrame
+print(components_df.shape)
+
+# Select row 3: component
+component = components_df.iloc[3]
+
+# Print result of nlargest
+print(component.nlargest())
