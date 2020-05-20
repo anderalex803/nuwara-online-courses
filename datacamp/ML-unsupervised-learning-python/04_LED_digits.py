f897c063c3caa16433fc60d474316d6acdf942a5
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-20 17:33:20 +0700

    Create 04_LED_digits.py

---
+++ datacamp/ML-unsupervised-learning-python/04_LED_digits.py
@@ -0,0 +1,49 @@
+"Display the LED digit dataset of Number 7"
+
+# Import pyplot
+from matplotlib import pyplot as plt
+
+# Select the 0th row: digit
+digit = samples[0,:]
+
+# Print digit
+print(digit)
+
+# Reshape digit to a 13x8 array: bitmap
+bitmap = digit.reshape(13,8)
+
+# Print bitmap
+print(bitmap)
+
+# Use plt.imshow to display bitmap
+plt.imshow(bitmap, cmap='gray', interpolation='nearest')
+plt.colorbar()
+plt.show()
+
+"NMF to encode Number 7"
+
+# Import NMF
+from sklearn.decomposition import NMF
+
+def show_as_image(sample):
+    bitmap = sample.reshape((13, 8))
+    plt.figure()
+    plt.imshow(bitmap, cmap='gray', interpolation='nearest')
+    plt.colorbar()
+    plt.show()
+
+# Create an NMF model: model
+model = NMF(n_components=7)
+
+# Apply fit_transform to samples: features
+features = model.fit_transform(samples)
+
+# Call show_as_image on each component
+for component in model.components_:
+    show_as_image(component)
+
+# Assign the 0th row of features: digit_features
+digit_features = features[0,:]
+
+# Print digit_features
+print(digit_features)
