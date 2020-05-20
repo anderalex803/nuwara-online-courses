069a84d72d51e797f165c6d01fa683d524078d44
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-21 00:21:27 +0700

    Create 03_logistic_regression_probabilities.py

---
+++ datacamp/ML-linear-classifier-python/03_logistic_regression_probabilities.py
@@ -0,0 +1,8 @@
+lr = LogisticRegression()
+lr.fit(X,y)
+
+# Get predicted probabilities
+proba = lr.predict_proba(X)
+
+# Sort the example indices by their maximum probability
+proba_inds = np.argsort(np.max(proba,axis=1))
