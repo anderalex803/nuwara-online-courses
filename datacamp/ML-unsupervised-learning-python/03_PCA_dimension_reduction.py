ceec0138e7af14f7ebdcf7a1c5ac4cfa75b9d80a
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-20 16:13:54 +0700

    Create 03_PCA_dimension_reduction.py

---
+++ datacamp/ML-unsupervised-learning-python/03_PCA_dimension_reduction.py
@@ -0,0 +1,15 @@
+# Import PCA
+from sklearn.decomposition import PCA
+
+# Create a PCA model with 2 components: pca
+pca = PCA(n_components=2)
+
+# Fit the PCA instance to the scaled samples
+# scaled samples are datasets after been scaled by StandardScaler, see the another python script
+pca.fit(scaled_samples)
+
+# Transform the scaled samples: pca_features
+pca_features = pca.transform(scaled_samples)
+
+# Print the shape of pca_features
+print(pca_features.shape)
