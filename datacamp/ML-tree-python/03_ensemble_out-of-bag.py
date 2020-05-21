2d1f488af240e5e113e8a15d5b3a159f96448378
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-21 19:47:23 +0700

    Create 03_ensemble_out-of-bag.py

---
+++ datacamp/ML-tree-python/03_ensemble_out-of-bag.py
@@ -0,0 +1,29 @@
+# Import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+# Import BaggingClassifier
+from sklearn.ensemble import BaggingClassifier
+
+# Instantiate dt
+dt = DecisionTreeClassifier(min_samples_leaf=8, random_state=1)
+
+# Instantiate bc with 50 trees and Out-of-Bag (OOB)
+bc = BaggingClassifier(base_estimator=dt, 
+            n_estimators=50,
+            oob_score=True,
+            random_state=1)
+
+# Fit bc to the training set 
+bc.fit(X_train, y_train)
+
+# Predict test set labels
+y_pred = bc.predict(X_test)
+
+# Evaluate test set accuracy
+acc_test = accuracy_score(y_test, y_pred)
+
+# Evaluate OOB accuracy
+acc_oob = bc.oob_score_
+
+# Print acc_test and acc_oob
+print('Test set accuracy: {:.3f}, OOB accuracy: {:.3f}'.format(acc_test, acc_oob))
