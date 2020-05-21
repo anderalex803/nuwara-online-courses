2e21bc2ec6950c2295822635fc4c3f49d76b7d85
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-22 00:39:30 +0700

    Create 01_ReLu_activation.py

---
+++ datacamp/ML-deep-learning/01_ReLu_activation.py
@@ -0,0 +1,15 @@
+def relu(input):
+    '''Define your relu activation function here'''
+    # Calculate the value for the output of the relu function: output
+    output = max(input, 0)
+    
+    # Return the value just calculated
+    return(output)
+
+# Calculate node 0 value: node_0_output
+node_0_input = (input_data * weights['node_0']).sum()
+node_0_output = relu(node_0_input)
+
+# Calculate node 1 value: node_1_output
+node_1_input = (input_data * weights['node_1']).sum()
+node_1_output = relu(node_1_input)
