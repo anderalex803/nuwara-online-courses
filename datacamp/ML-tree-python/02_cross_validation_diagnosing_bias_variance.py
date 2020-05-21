4e14efd50fc4293d9073e31b33960b5bce789dfc
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-21 17:50:30 +0700

    Create 02_cross_validation_diagnosing_bias_variance.py

---
+++ datacamp/ML-tree-python/02_cross_validation_diagnosing_bias_variance.py
@@ -0,0 +1,47 @@
+"Step 1. Instantiate an ML model: DecisionTree"
+
+# Import train_test_split from sklearn.model_selection
+from sklearn.model_selection import train_test_split
+
+# Set SEED for reproducibility
+SEED = 1
+
+# Split the data into 70% train and 30% test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=SEED)
+
+# Instantiate a DecisionTreeRegressor dt
+dt = DecisionTreeRegressor(min_samples_leaf=0.26, max_depth=4, random_state=SEED)
+
+"Step 2. Compute Root Mean Squared error mean from CV"
+
+from sklearn.model_selection import cross_val_score
+
+# Compute the array containing the 10-folds CV MSEs
+MSE_CV_scores = - cross_val_score(dt, X_train, y_train, cv=10, 
+                       scoring='neg_mean_squared_error',
+                       n_jobs=-1)
+
+# Compute the 10-folds CV RMSE
+RMSE_CV = (MSE_CV_scores.mean())**(1/2)
+
+# Print RMSE_CV
+print('CV RMSE: {:.2f}'.format(RMSE_CV))
+
+"Step 3. Compute RMSE of the train set"
+
+# Import mean_squared_error from sklearn.metrics as MSE
+from sklearn.metrics import mean_squared_error as MSE
+
+# Fit dt to the training set
+dt.fit(X_train, y_train)
+
+# Predict the labels of the training set
+y_pred_train = dt.predict(X_train)
+
+# Evaluate the training set RMSE of dt
+RMSE_train = (MSE(y_train, y_pred_train))**(1/2)
+
+# Print RMSE_train
+print('Train RMSE: {:.2f}'.format(RMSE_train))
+
+"Step 4. Decide if it has high variance or high bias"
