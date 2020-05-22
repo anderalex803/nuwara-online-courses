ef662958aa81b0f4f1bab464e29e3394d2b91c17
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-22 23:16:39 +0700

    Update 03_kfold.py

--- datacamp/ML-model-validation/03_kfold.py
+++ datacamp/ML-model-validation/03_kfold.py
@@ -10,3 +10,19 @@ splits = kf.split(X)
 for train_index, val_index in splits:
     print("Number of training indices: %s" % len(train_index))
     print("Number of validation indices: %s" % len(val_index))
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+rfc = RandomForestRegressor(n_estimators=25, random_state=1111)
+
+# Access the training and validation indices of splits
+for train_index, val_index in splits:
+    # Setup the training and validation data
+    X_train, y_train = X[train_index], y[train_index]
+    X_val, y_val = X[val_index], y[val_index]
+    # Fit the random forest model
+    rfc.fit(X_train, y_train)
+    # Make predictions, and print the accuracy
+    predictions = rfc.predict(X_val)
+    print("Split accuracy: " + str(mean_squared_error(y_val, predictions)))
