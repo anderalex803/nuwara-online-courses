f9541973b2d161dc44cf6f6b8767723eaab0c107
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-22 22:35:02 +0700

    Create 02_overfit_or_underfit.py

---
+++ datacamp/ML-model-validation/02_overfit_or_underfit.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+
+test_scores, train_scores = [], []
+for i in [1, 2, 3, 4, 5, 10, 20, 50]:
+    rfc = RandomForestClassifier(n_estimators=i, random_state=1111)
+    rfc.fit(X_train, y_train)
+    # Create predictions for the X_train and X_test datasets.
+    train_predictions = rfc.predict(X_train)
+    test_predictions = rfc.predict(X_test)
+    # Append the accuracy score for the test and train predictions.
+    train_scores.append(round(accuracy_score(y_train, train_predictions), 2))
+    test_scores.append(round(accuracy_score(y_test, test_predictions), 2))
+# Print the train and test scores.
+print("The training scores were: {}".format(train_scores))
+print("The testing scores were: {}".format(test_scores))
+
+plt.plot([1, 2, 3, 4, 5, 10, 20, 50], test_scores)
+plt.plot([1, 2, 3, 4, 5, 10, 20, 50], train_scores)
+plt.show()
