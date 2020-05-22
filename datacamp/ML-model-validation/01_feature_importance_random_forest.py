9a0511e215543c46fcb07572bf26cd07e28acb70
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-22 17:58:20 +0700

    Create 01_feature_importance_random_forest.py

---
+++ datacamp/ML-model-validation/01_feature_importance_random_forest.py
@@ -0,0 +1,21 @@
+rfr = RandomForestRegressor()
+
+# Set the number of trees
+rfr.n_estimators = 100
+
+# Add a maximum depth
+rfr.max_depth = 6
+
+# Set the random state
+rfr.random_state = 1111
+
+# Fit the model
+rfr.fit(X_train, y_train)
+
+# Fit the model using X and y
+rfr.fit(X_train, y_train)
+
+# Print how important each column is to the model
+for i, item in enumerate(rfr.feature_importances_):
+      # Use i and item to print out the feature importance of each column
+    print("{0:s}: {1:.2f}".format(X_train.columns[i], item))
