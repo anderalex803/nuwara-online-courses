d842d3f16aef21c9458d8922aac0e3ff3a01c746
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-22 21:43:13 +0700

    Create 02_metric_score.py

---
+++ datacamp/ML-model-validation/02_metric_score.py
@@ -0,0 +1,9 @@
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
+test_predictions = rfc.predict(X_test)
+
+# Create precision or recall score based on the metric you imported
+score = precision_score(y_test, test_predictions)
+
+# Print the final result
+print("The precision value is {0:.2f}".format(score))
