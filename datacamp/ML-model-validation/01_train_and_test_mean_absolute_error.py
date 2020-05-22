07de6614cc1842ba5c2c4a39f96943412744592a
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-22 17:49:23 +0700

    Create 01_train_and_test_mean_absolute_error.py

---
+++ datacamp/ML-model-validation/01_train_and_test_mean_absolute_error.py
@@ -0,0 +1,16 @@
+from sklearn.metrics import mean_absolute_error as mae
+
+# The model is fit using X_train and y_train
+model.fit(X_train, y_train)
+
+# Create vectors of predictions
+train_predictions = model.predict(X_train)
+test_predictions = model.predict(X_test)
+
+# Train/Test Errors
+train_error = mae(y_true=y_train, y_pred=train_predictions)
+test_error = mae(y_true=y_test, y_pred=test_predictions)
+
+# Print the accuracy for seen and unseen data
+print("Model error on seen data: {0:.2f}.".format(mae(y_train, train_predictions)))
+print("Model error on unseen data: {0:.2f}.".format(mae(y_test, test_predictions)))
