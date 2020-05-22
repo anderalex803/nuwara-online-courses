94e15ff71730e97d92885b7990bfd70e2a33ca9c
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-22 19:49:35 +0700

    Create 02_mean_absolute_error.py

---
+++ datacamp/ML-model-validation/02_mean_absolute_error.py
@@ -0,0 +1,10 @@
+from sklearn.metrics import mean_absolute_error
+
+# Manually calculate the MAE
+n = len(predictions)
+mae_one = sum(abs(y_test - predictions)) / n
+print('With a manual calculation, the error is {}'.format(mae_one))
+
+# Use scikit-learn to calculate the MAE
+mae_two = mean_absolute_error(y_test, predictions)
+print('Using scikit-lean, the error is {}'.format(mae_two))
