cdccbb095b6e0130c5e8cbc6c4544ae967f70c3d
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-22 19:26:42 +0700

    Create 01_print_model_details.py

---
+++ datacamp/ML-model-validation/01_print_model_details.py
@@ -0,0 +1,12 @@
+# Fit the rfc model. 
+rfc.fit(X_train, y_train)
+
+# Create arrays of predictions
+classification_predictions = rfc.predict(X_test)
+probability_predictions = rfc.predict_proba(X_test)
+
+# Print out count of binary predictions
+print(pd.Series(classification_predictions).value_counts())
+
+# Print the first value from probability_predictions
+print('The first predicted probabilities are: {}'.format(probability_predictions[0]))
