e88b342051c21ae3c2b2e91a43dd999a0e6d37c5
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-22 21:39:47 +0700

    Create 02_confusion_matrix.py

---
+++ datacamp/ML-model-validation/02_confusion_matrix.py
@@ -0,0 +1,11 @@
+from sklearn.metrics import confusion_matrix
+
+# Create predictions
+test_predictions = rfc.predict(X_test)
+
+# Create and print the confusion matrix
+cm = confusion_matrix(y_test, test_predictions)
+print(cm)
+
+# Print the true positives (actual 1s that were predicted 1s)
+print("The number of true positives is: {}".format(cm[1, 1]))
