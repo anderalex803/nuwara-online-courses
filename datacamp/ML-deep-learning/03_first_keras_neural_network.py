18a95db1ec7b0cefad532dc559b518555626b0fb
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-22 14:23:19 +0700

    Update 03_first_keras_neural_network.py

--- datacamp/ML-deep-learning/03_first_keras_neural_network.py
+++ datacamp/ML-deep-learning/03_first_keras_neural_network.py
@@ -1,4 +1,4 @@
-"Make neural network with 50 nodes on outer layer, 32 nodes on hidden layer, with Sequential model"
+"Step 1. Make neural network with 50 nodes on outer layer, 32 nodes on hidden layer, with Sequential model"
 
 # Import necessary modules
 import keras
@@ -19,3 +19,16 @@ model.add(Dense(32, activation='relu', input_shape=(n_cols,)))
 
 # Add the output layer
 model.add(Dense(1, input_shape=(n_cols,)))
+
+"Step 2. Compile the model"
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Verify that model contains information from compiling
+print("Loss function: " + model.loss)
+
+"Step 3. Fit the model to the predictor and target data"
+
+# Fit the model
+model.fit(predictors, target)
