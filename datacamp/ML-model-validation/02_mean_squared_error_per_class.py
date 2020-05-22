301e498994e53f22edf40b7061bc5f6909f3f2bd
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-22 19:58:37 +0700

    Create 02_mean_squared_error_per_class.py

---
+++ datacamp/ML-model-validation/02_mean_squared_error_per_class.py
@@ -0,0 +1,13 @@
+# Find the East conference teams
+east_teams = labels == "E"
+
+# Create arrays for the true and predicted values
+true_east = y_test[east_teams]
+preds_east = predictions[east_teams]
+
+# Print the accuracy metrics
+print('The MAE for East teams is {}'.format(
+    mae(true_east, preds_east)))
+
+# Print the West accuracy
+print('The MAE for West conference is {}'.format(west_error))
