44687e83dd4f608a98aba56400d10745ac4879fd
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-05-22 19:51:33 +0700

    Create 02_mean_squared_error.py

---
+++ datacamp/ML-model-validation/02_mean_squared_error.py
@@ -0,0 +1,10 @@
+from sklearn.metrics import mean_squared_error
+
+n = len(predictions)
+# Finish the manual calculation of the MSE
+mse_one = sum((y_test - predictions)**2) / n
+print('With a manual calculation, the error is {}'.format(mse_one))
+
+# Use the scikit-learn function to calculate MSE
+mse_two = mean_squared_error(y_test, predictions)
+print('Using scikit-lean, the error is {}'.format(mse_two))
