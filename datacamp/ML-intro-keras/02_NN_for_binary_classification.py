bdfd7ad2839c0e2e27abd2285037b411ad4d7571
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-09-03 21:12:29 +0700

    Create 02_NN_for_binary_classification.py

---
+++ datacamp/ML-intro-keras/02_NN_for_binary_classification.py
@@ -0,0 +1,24 @@
+# Import the sequential model and dense layer
+from keras.layers import Dense
+from keras.models import Sequential
+
+# Create a sequential model
+model = Sequential()
+
+# Add a dense layer 
+model.add(Dense(1, input_shape=(4,), activation='sigmoid'))
+
+# Compile your model
+model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
+
+# Display a summary of your model
+model.summary()
+
+# Train your model for 20 epochs
+model.fit(X_train, y_train, epochs = 20)
+
+# Evaluate your model accuracy on the test set
+accuracy = model.evaluate(X_test, y_test)[1]
+
+# Print accuracy
+print('Accuracy:', accuracy)
